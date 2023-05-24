2f8284bd2db787e36034935abfd3733524f0a05c
Soccer9541 <sejin9541@naver.com>
2023-05-24 16:21:33 +0900

    캐러셀기능

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,38 +1,21 @@
-import styles from './Carousel.module.css';
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import Start from "./Start";
+import data from "./HomeData";
 
-function Carousel(){
-  return (
-    <div className="carousel w-full">
-      <div id="slide1" className="carousel-item relative w-full">
-        <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide4" className="btn btn-circle">❮</a> 
-          <a href="#slide2" className="btn btn-circle">❯</a>
-        </div>
-      </div> 
-      <div id="slide2" className="carousel-item relative w-full">
-        <img src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">❮</a> 
-          <a href="#slide3" className="btn btn-circle">❯</a>
-        </div>
-      </div> 
-      <div id="slide3" className="carousel-item relative w-full">
-        <img src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">❮</a> 
-          <a href="#slide4" className="btn btn-circle">❯</a>
-        </div>
-      </div> 
-      <div id="slide4" className="carousel-item relative w-full">
-        <img src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">❮</a> 
-          <a href="#slide1" className="btn btn-circle">❯</a>
-        </div>
-      </div>
-    </div>
+
+function CarouselArea(){
+  let 캐러셀data = data;
+  return(
+    <Carousel infiniteLoop autoPlay interval="2000" transitionTime="700" showThumbs={false}>
+
+      {캐러셀data.map((a,i)=>{
+        return(
+          <Start 캐러셀data={캐러셀data[i]} key={i}></Start>
+        )
+      })}
+    </Carousel>
   )
-};
+}
 
-export default Carousel;
+export default CarouselArea;
