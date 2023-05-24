e43a77c400b8391f856f900852ce201f34dd26b4
Soccer9541 <sejin9541@naver.com>
2023-05-24 16:27:10 +0900

    글 수정 화면

--- src/routes/Modify.js
+++ src/routes/Modify.js
@@ -17,6 +17,8 @@ function Modify(){
   let navigate = useNavigate();
   const [imageSrc, setImageSrc] = useState(process.env.PUBLIC_URL + '/images/photoDefualt.gif');
 
+  let [박스추가,박스추가설정] = useState([]);
+  let [단계별내용,단계별내용설정] = useState([]);
 
   let 정보가져오기 = async function(){
     const 컬렉션 = collection(db, "post");
@@ -28,6 +30,8 @@ function Modify(){
       document.querySelector('#category').value= result.data().category;
       document.querySelector('#content').value = result.data().content;
       document.querySelector('#imgArea img').src = result.data().사진;
+      단계별내용설정(result.data().단계별내용);
+      박스추가설정(result.data().단계별추가사진);
     })
 
     
@@ -69,13 +73,25 @@ function Modify(){
     let fileImage = document.querySelector('#recipePhoto').files[0];
     let 날짜 = new Date();
     const 수정문서 = doc(db, "post", postDocID);
-    console.log(postDocID);
-    console.log(수정문서);
-    console.log(날짜);
+
     if(fileImage==null){
-      alert('수정된 이미지를 업로드하셈 ㅅㄱ');
+      updateDoc(수정문서, {
+        title : document.querySelector('#title').value,
+        category : document.querySelector('#category').value,
+        content : document.querySelector('#content').value,
+        작성자 : auth.currentUser.uid,
+        수정일 : `${날짜.getFullYear()}년 ${날짜.getMonth()+1}월 ${날짜.getDate()}일`,
+        단계별추가사진 : 박스추가,
+        단계별내용 : 단계별내용
+      })
+      .then(()=>{
+        alert('글 수정 완료!');
+        setImageSrc(process.env.PUBLIC_URL + '/images/photoDefualt.gif');
+        navigate(`/detail?id=${postDocID}`);
+      });
       return
     }
+
     try {
       await 요리사진(fileImage)
       .then((result)=>{
@@ -86,7 +102,9 @@ function Modify(){
           content : document.querySelector('#content').value,
           작성자 : auth.currentUser.uid,
           수정일 : `${날짜.getFullYear()}년 ${날짜.getMonth()+1}월 ${날짜.getDate()}일`,
-          사진 : result
+          사진 : result,
+          단계별추가사진 : 박스추가,
+          단계별내용 : 단계별내용
         });
       })
       .then(()=>{
@@ -102,6 +120,74 @@ function Modify(){
     }
   }
 
+    let 단계추가 = async function(){
+    console.log('단계추가용');
+    let copy =[...박스추가];
+    copy.push('');
+    박스추가설정(copy);
+
+    let copy2 =[...단계별내용];
+    copy2.push('');
+    단계별내용설정(copy2);
+  }
+
+
+  let 단계추가삭제 = (e,i)=>{
+    e.preventDefault();
+    console.log(i)
+    console.log('삭제');
+    let copy=[...박스추가];
+    copy.splice(i,1);
+    박스추가설정(copy);
+
+    let copy2=[...단계별내용];
+    copy2.splice(i,1);
+    단계별내용설정(copy2);
+
+    document.getElementsByClassName('contentRecipe')[i].value='';
+    document.getElementsByClassName('previewPhoto')[i].value='';
+  }
+
+
+  let 요리단계사진추가 = async (e,i) =>{
+    e.preventDefault();
+    const file=e.target.files[0];
+    console.log(i)
+    console.log(file);
+    if(file==null){
+      let copy=[...박스추가];
+      copy[i]='';
+      박스추가설정(copy);
+      console.log('test');
+      return
+    }
+    const reader = new FileReader();
+    reader.onloadend = () => {};
+    reader.readAsDataURL(file);
+    try{
+      await 요리사진(file)
+      .then((result)=>{
+        let copy=[...박스추가];
+        copy[i]=result;
+        박스추가설정(copy);
+        
+      })
+    }
+    catch(e){
+      console.error("Error adding document: ", e);
+    }
+    
+
+  }
+
+  let 요리단계글추가 = (e,i) =>{
+    e.preventDefault();
+    let copy = [...단계별내용];
+    copy[i] = e.target.value;
+    단계별내용설정(copy);
+    
+  }
+
 
 
   useEffect(()=>{
@@ -109,15 +195,17 @@ function Modify(){
     
   },[])
   
-
+  useEffect(()=>{
+    console.log('test')
+    console.log(박스추가);
+    console.log(단계별내용);
+  })
 
   return (
     <div>
       {postDocID}
-      <div>
-        
-      </div>
-      <form action="" className={styles.modifyArea}>
+      
+      <form className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100" id={styles.modifyArea}>
         <h1 className={styles.title}>수정 페이지임</h1>
         <div>
           <p>제목</p>
@@ -149,7 +237,41 @@ function Modify(){
           <p>내용</p>
           <textarea name="content" id="content" placeholder='글 내용' cols="30" rows="10"></textarea>
         </div>
+        <div style={{marginBottom : '20px'}}>
+          <button className='btn btn-outline' onClick={(e)=>{e.preventDefault();단계추가()}}>단계추가하기</button>
+        </div>
+        <hr />
         <div>
+          {
+            박스추가 !== undefined ?
+            박스추가.map((a,i)=>{
+
+              return(
+                <div key={i}>
+                  <div className='flex mt-4 mb-4 items-center'>
+                    <p className={styles.step}>STEP{i+1}</p>
+                    <div className='grow'></div>
+                    <button className="btn btn-square btn-outline" onClick={(e)=>{단계추가삭제(e,i)}}>
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                    </button>
+                  </div>
+                  <input type="file" accept="image/*" className='previewPhoto' onChange={(e)=>{요리단계사진추가(e,i)}}  />
+                  <div className={styles.addPhoto}>
+                    {
+                      process.env.PUBLIC_URL + '/images/addPhoto.gif' &&
+                      <img src={박스추가[i] ? 박스추가[i]: process.env.PUBLIC_URL + '/images/addPhoto.gif'} alt="Preview" />
+                    }
+                  </div>
+                  <textarea name="" value={단계별내용[i]} cols="30" rows="10" className='contentRecipe' onChange={(e)=>{요리단계글추가(e,i)}}></textarea>
+                  <hr />
+                </div>
+              )
+            })
+            :
+            null
+          }
+        </div>
+        <div style={{marginTop : '20px'}}>
           <button type="submit" id='send' className="btn btn-outline" onClick={(e)=>{e.preventDefault();수정()}}>글 수정</button>
         </div>
       </form>
