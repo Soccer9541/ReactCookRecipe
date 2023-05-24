cc0762992b315740dafc30272b1689f6b2b8c070
Soccer9541 <sejin9541@naver.com>
2023-05-24 16:22:46 +0900

    메뉴페이지

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -6,10 +6,10 @@ function MenuItem(props){
   let navigate = useNavigate();
 
   return(
-    <div className="card w-96 bg-base-100 shadow-xl sm:w-1/2 md:w-1/3 lg:w-1/4 p-4" onClick={()=>{navigate(`/recipe/${props.route}`)}}>
+    <div id={styles.cardItem} className="card w-96 bg-base-100 shadow-xl sm:w-1/2 md:w-1/3 lg:w-1/4 p-4" onClick={()=>{navigate(`/recipe/${props.route}`)}}>
       <figure><img src={props.img} alt="Shoes" style={{borderBottom : '1px solid black'}} /></figure>
       <div className="card-body">
-        <h2 className="card-title justify-center">
+        <h2 className="card-title justify-center text-xl">
           {props.title}
         </h2>
       </div>
