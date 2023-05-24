13321949c919b2b35e5d37e8e367aa45df62beda
Soccer9541 <sejin9541@naver.com>
2023-05-24 16:22:16 +0900

    캐러셀데이터

---
+++ src/components/HomeData.js
@@ -0,0 +1,20 @@
+let data=[
+  {
+    head : '누구든 쉽게!',
+    content : "집에서 느끼는 따뜻한 맛, 집밥의 민족과 함께하세요.",
+    backgroundImage : process.env.PUBLIC_URL+ '/images/home2.jpg'
+  },
+  {
+    head : '행복한 맛을 가득',
+    content : "맛있고 건강한 집밥 레시피를 찾고 계신가요? 지금 시작해보세요.",
+    backgroundImage : process.env.PUBLIC_URL+'/images/home1.jpg'
+  },
+  {
+    head : '언제 어디서든 간편하게',
+    content : "집에서 즐기는 최고의 식사를 집밥의 민족과 함께 완성해보세요!",
+    backgroundImage :  process.env.PUBLIC_URL+'/images/home3.jpg'
+  }
+]
+
+export default data;
+
